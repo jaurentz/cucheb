c606582db859b509727390aeaeeb2a4543dcebc8
Jared Aurentz <aurentz@maths.ox.ac.uk>
2014-11-12 20:08:36 +0000

    cleaned out complex subroutines

--- src/complex_double/cuchebZmult.cu
+++
@@ -1,5 +0,0 @@
-#include <cucheb.h>
-
-cuchebStatus_t cuchebZmult(int n,cuDoubleComplex* x,cuDoubleComplex* y,cuchebOpMult opmult,void* userdata,ChebPoly* chebpoly){
-	return CUCHEB_STATUS_SUCCESS;
-}
