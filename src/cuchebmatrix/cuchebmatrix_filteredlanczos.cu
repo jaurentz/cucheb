b97b2c20833f0db2c96553fb8cb9300da2509f67
Jared <jaredaurentz@gmail.com>
2016-09-08 20:25:47 -0500

    added descriptions to user level routines

--- src/cuchebmatrix/cuchebmatrix_filteredlanczos.cu
+++ src/cuchebmatrix/cuchebmatrix_filteredlanczos.cu
@@ -1,4 +1,18 @@
 #include <cucheb.h>
+/*
+  cuchebmatrix_init
+
+  This routine computes the all the eigenvalues of a matrix in user-specified
+  interval using the filtered Lanczos procedure. The following inputs are
+  required:
+
+    lbnd  - the lower bound of the desired interval
+    ubnd  - the upper bound of the desired interval
+    bsize - the size of the Lanczos blocks
+    ccm   - a reference to an initialized instance of a cuchebmatrix
+    ccl   - a reference to an uninitialized instance of a cucheblanczos
+
+*/
 
 /* filtered lanczos routine for interval */
 int cuchebmatrix_filteredlanczos(double lbnd, double ubnd, int bsize, 
